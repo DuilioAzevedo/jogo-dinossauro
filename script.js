b2aee68e659342c8cfe6f008b09956004bf2c40c
DuilioAzevedo <duilio_uei@hotmail.com>
2021-09-16 16:00:48 -0300

    part_2

---
+++ script.js
@@ -0,0 +1,35 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+      if (!isJumping) {
+        jump();
+      }
+    }
+  }
+  
+  function jump() {
+    isJumping = true;
+  
+    let upInterval = setInterval(() => {
+      if (position >= 150) {
+        // Descendo
+        clearInterval(upInterval);
+  
+        let downInterval = setInterval(() => {
+          if (position <= 0) {
+            clearInterval(downInterval);
+            isJumping = false;
+          } else {
+            position -= 20;
+            dino.style.bottom = position + 'px';
+          }
+        }, 20);
+      } else {
+        // Subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+      }
+    }, 20);
+  }
+  document.addEventListener('keyup', handleKeyUp);
